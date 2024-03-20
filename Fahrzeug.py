8904f11edcf6bcebc206cc78a00882eec5c27b1b
Jeremy Thelen <j.thelen@billiger-mietwagen.de>
2024-03-20 12:22:23 +0100

    add crud methods to classes

--- Fahrzeug.py
+++ Fahrzeug.py
@@ -1,3 +1,5 @@
+import mysql.connector
+
 class Fahrzeug:
     def __init__(self, FahrzeugID, Art, Modell, Nummernschild_Rahmennummer, Baujahr, Typ, Treibstoff):
         self.FahrzeugID = FahrzeugID
@@ -7,3 +9,38 @@ class Fahrzeug:
         self.Baujahr = Baujahr
         self.Typ = Typ
         self.Treibstoff = Treibstoff
+
+    @staticmethod
+    def create(connection, Art, Modell, Nummernschild_Rahmennummer, Baujahr, Typ, Treibstoff):
+        cursor = connection.cursor()
+        sql = "INSERT INTO Fahrzeuge (Art, Modell, Nummernschild_Rahmennummer, Baujahr, Typ, Treibstoff) VALUES (%s, %s, %s, %s, %s, %s)"
+        val = (Art, Modell, Nummernschild_Rahmennummer, Baujahr, Typ, Treibstoff)
+        cursor.execute(sql, val)
+        connection.commit()
+        return cursor.lastrowid
+
+    @staticmethod
+    def read(connection, FahrzeugID):
+        cursor = connection.cursor()
+        sql = "SELECT * FROM Fahrzeuge WHERE FahrzeugID = %s"
+        val = (FahrzeugID,)
+        cursor.execute(sql, val)
+        result = cursor.fetchone()
+        if result:
+            return Fahrzeug(*result)
+        else:
+            return None
+
+    def update(self, connection):
+        cursor = connection.cursor()
+        sql = "UPDATE Fahrzeuge SET Art = %s, Modell = %s, Nummernschild_Rahmennummer = %s, Baujahr = %s, Typ = %s, Treibstoff = %s WHERE FahrzeugID = %s"
+        val = (self.Art, self.Modell, self.Nummernschild_Rahmennummer, self.Baujahr, self.Typ, self.Treibstoff, self.FahrzeugID)
+        cursor.execute(sql, val)
+        connection.commit()
+
+    def delete(self, connection):
+        cursor = connection.cursor()
+        sql = "DELETE FROM Fahrzeuge WHERE FahrzeugID = %s"
+        val = (self.FahrzeugID,)
+        cursor.execute(sql, val)
+        connection.commit()
--- Inspektion.py
+++ Inspektion.py
@@ -1,5 +1,42 @@
+import mysql.connector
+
 class Inspektion:
     def __init__(self, InspektionID, FahrzeugID, Datum_letzte_Inspektion):
         self.InspektionID = InspektionID
         self.FahrzeugID = FahrzeugID
         self.Datum_letzte_Inspektion = Datum_letzte_Inspektion
+
+    @staticmethod
+    def create(connection, FahrzeugID, Datum_letzte_Inspektion):
+        cursor = connection.cursor()
+        sql = "INSERT INTO Inspektionen (FahrzeugID, Datum_letzte_Inspektion) VALUES (%s, %s)"
+        val = (FahrzeugID, Datum_letzte_Inspektion)
+        cursor.execute(sql, val)
+        connection.commit()
+        return cursor.lastrowid
+
+    @staticmethod
+    def read(connection, InspektionID):
+        cursor = connection.cursor()
+        sql = "SELECT * FROM Inspektionen WHERE InspektionID = %s"
+        val = (InspektionID,)
+        cursor.execute(sql, val)
+        result = cursor.fetchone()
+        if result:
+            return Inspektion(*result)
+        else:
+            return None
+
+    def update(self, connection):
+        cursor = connection.cursor()
+        sql = "UPDATE Inspektionen SET FahrzeugID = %s, Datum_letzte_Inspektion = %s WHERE InspektionID = %s"
+        val = (self.FahrzeugID, self.Datum_letzte_Inspektion, self.InspektionID)
+        cursor.execute(sql, val)
+        connection.commit()
+
+    def delete(self, connection):
+        cursor = connection.cursor()
+        sql = "DELETE FROM Inspektionen WHERE InspektionID = %s"
+        val = (self.InspektionID,)
+        cursor.execute(sql, val)
+        connection.commit()
