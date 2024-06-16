0e01ffa4f490abdece9b6963d315707038bb683c
JasonHoyerGit <hoyerjason1@gmail.com>
2024-06-16 13:57:57 +0200

    Variabelen und Methode zu static umgewandelt

--- client/Auth.py
+++ client/Auth.py
@@ -1,10 +1,13 @@
 import base64
 
-
 class Auth:
+    __username = ""
+    __password = ""
     def __init__(self, username, password):
-        self.username = username
-        self.password = password
+        Auth.__username = username
+        Auth.__password = password
 
-    def getAuth(self):
-        return "Basic " + str(base64.b64encode(str(self.username + ':' + self.password).encode("UTF-8")))
+    @staticmethod
+    def getAuth():
+        return (Auth.__username, Auth.__password)
+        #return "Basic " + str(base64.b64encode(str(self.username + ':' + self.password).encode("UTF-8")))
