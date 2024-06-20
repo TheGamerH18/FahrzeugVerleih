fde68305ba07a01ef3acb9577a6b0c4b2345cbc6
JasonHoyerGit <hoyerjason1@gmail.com>
2024-06-20 10:29:21 +0200

    Dialog (LoginDialog) now shows up as a Dialog

--- client/client.py
+++ client/client.py
@@ -1,6 +1,55 @@
-import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QPushButton, QLabel, QStackedWidget, QMainWindow
 from BauartenWidget import *
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QPushButton, QLabel, QStackedWidget, QMainWindow, QMessageBox, QLineEdit, QDialog
+
+class LoginDialog(QDialog):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        button_style = """
+            QPushButton {
+                    background-color: #4CAF50; 
+                    color: white;
+                    border-radius: 10px;
+                    font-size: 16px;
+                    padding: 10px;
+                }
+                QPushButton:hover {
+                    background-color: #45a049;
+                }
+                """
+        self.setWindowTitle("Login")
+        self.setFixedWidth(500)
+        self.setFixedHeight(250)
+        self.geometry().center()
+
+        self.tbxBenutzername = QLineEdit()
+        self.tbxBenutzername.setPlaceholderText("Benutzername")
+        self.tbxBenutzername.setFixedHeight(30)
+        self.tbxPasswort = QLineEdit()
+        self.tbxPasswort.setPlaceholderText("Passwort")
+        self.tbxPasswort.setFixedHeight(30)
+
+        self.btnLogin = QPushButton("Login")
+        self.btnLogin.clicked.connect(self.login)
+
+        layout = QVBoxLayout()
+        layout.addWidget(self.tbxBenutzername)
+        layout.addWidget(self.tbxPasswort)
+        layout.addWidget(self.btnLogin)
+
+        for i in range(layout.count()):
+            layout.itemAt(i).widget().setStyleSheet(button_style)
+
+        self.setLayout(layout)
+
+    def login(self):
+            Auth(self.tbxBenutzername.text(), self.tbxPasswort.text())
+            print(Auth.getAuth())
+            self.close()
+
 
 class MenueWidget(QWidget):
     def __init__(self):
@@ -43,6 +92,9 @@ class MenueWidget(QWidget):
 
         self.setLayout(layout)
 
+        dialog = LoginDialog()
+        dialog.exec_()
+
     def show_bauarten(self):
         self.bauarten_widget = BauartenWidget()
         self.bauarten_widget.show()
@@ -53,6 +105,8 @@ if __name__ == '__main__':
     widget = MenueWidget()
     widget.show()
     sys.exit(app.exec_())
+
+
 import os
 import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QWidget, QPushButton, QDialog, QLabel
